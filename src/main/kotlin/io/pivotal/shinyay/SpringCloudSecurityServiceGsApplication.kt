4a57298a5cb82799bbbe0b1cb0cca630cd263159
shinyay <shinya.com@gmail.com>
2019-09-25 07:27:27 +0900

    Add logger with logbak over slf4j

--- src/main/kotlin/io/pivotal/shinyay/SpringCloudSecurityServiceGsApplication.kt
+++ src/main/kotlin/io/pivotal/shinyay/SpringCloudSecurityServiceGsApplication.kt
@@ -1,10 +1,14 @@
 package io.pivotal.shinyay
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringCloudSecurityServiceGsApplication
+class SpringCloudSecurityServiceGsApplication {
+	val logger: Logger = LoggerFactory.getLogger(this::class.java.name)
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringCloudSecurityServiceGsApplication>(*args)
