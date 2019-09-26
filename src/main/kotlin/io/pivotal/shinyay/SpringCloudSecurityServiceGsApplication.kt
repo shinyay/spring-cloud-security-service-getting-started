4036dd2b363b1fbcca59e570779709f18f95faca
shinyay <shinya.com@gmail.com>
2019-09-27 00:03:03 +0900

    Add to access JWT data

--- src/main/kotlin/io/pivotal/shinyay/SpringCloudSecurityServiceGsApplication.kt
+++ src/main/kotlin/io/pivotal/shinyay/SpringCloudSecurityServiceGsApplication.kt
@@ -4,10 +4,22 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.web.bind.annotation.GetMapping
 
 @SpringBootApplication
 class SpringCloudSecurityServiceGsApplication(val logger: Logger) {
 //	val logger: Logger = LoggerFactory.getLogger(this::class.java.name)
+
+	@GetMapping("/resource")
+	fun resource(@AuthenticationPrincipal jwt: Jwt): String {
+		logger.trace("***** JWT HEADERS: ${jwt.headers}")
+		logger.trace("***** JWT CLAIMS: ${jwt.claims}")
+		logger.trace("***** JWT TOKEN: ${jwt.tokenValue}")
+
+		return "Resource accessed by: ${jwt.claims["user_name"]} (with subjectId: ${jwt.subject})"
+	}
 }
 
 fun main(args: Array<String>) {
